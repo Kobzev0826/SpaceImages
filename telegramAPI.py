ddb55de15893e019544ced4a1521328f96296cf3
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2022-12-24 00:27:24 +0300

    add upload_pictures_in_channel.py - infinite loop with delay

--- telegramAPI.py
+++ telegramAPI.py
@@ -1,24 +1,42 @@
 import os, telegram, sys
+import time
+import random
+
 from dotenv import load_dotenv
 
 
-CHAT_ID = -1001790326720
 
 
-def read_token():
+
+def read_from_enviroment(name):
   try:
     load_dotenv()
-    token = os.environ["TELEGRAM_BOT_TOKEN"]
+    token = os.environ[f"{name}"]
   except:
     try:
-      token = os.environ["TELEGRAM_BOT_TOKEN"]
+      token = os.environ[f"{name}"]
     except KeyError as e:
       print(f"Error: {e} \n no token in system environment")
       sys.exit()
   return token
 
 
+def send_photo(bot,photo_path):
+    bot.send_photo(chat_id=get_chat_id(), photo=open(f'{photo_path}', 'rb'))
+
+
+def get_chat_id():
+  bot = telegram.Bot(token=read_from_enviroment('TELEGRAM_BOT_TOKEN'))
+  updates = bot.get_updates()
+  ch_usrname = read_from_enviroment('CH_USERNAME')
+  for update in updates:
+    if update['my_chat_member']:
+      if update['my_chat_member']['chat']['username'] == ch_usrname:
+        return update['my_chat_member']['chat']['id']
+      else:
+        print(f"I don't see a channel with username={ch_usrname}")
+        sys.exit()
+
+
 if __name__ == '__main__':
-    bot = telegram.Bot(token=read_token())
-    # bot.send_message(text='Test msg!', chat_id=CHAT_ID)
-    bot.send_photo(chat_id = CHAT_ID, photo = open('nasa_epic/epic_1b_20221221001752.png','rb'))
+    pass
---
+++ upload_pictures_in_channel.py
@@ -0,0 +1,23 @@
+import os, telegram, sys
+import time
+import random
+import telegramAPI
+
+
+def get_all_pictures_path_from_dir(dir_path):
+    pictures = []
+    for root, dirs, files in os.walk(dir_path):
+      for file in files:
+        if file.endswith(".png") or file.endswith(".jpg"):
+          pictures.append(os.path.join(root, file))
+    return pictures
+
+
+if __name__ == '__main__':
+    bot = telegram.Bot(token=telegramAPI.read_from_enviroment('TELEGRAM_BOT_TOKEN'))
+    while True:
+      pictures = get_all_pictures_path_from_dir(f'{os.getcwd()}')
+      random.shuffle(pictures)
+      while pictures:
+        telegramAPI.send_photo(bot, pictures.pop() )
+        time.sleep(telegramAPI.read_from_enviroment('PAUSE'))
