cd450746dc8a3e1096f4e05f60593fd6128a4fcc
Vladimir Kobzev <93667972+Kobzev0826@users.noreply.github.com>
2022-12-26 23:13:33 +0300

    fetch_spacex_images.py add default value for arg dir

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     load_dotenv()
     parser = argparse.ArgumentParser(description="download images from spacexdata.com")
     parser.add_argument('-id', help='id of launch if None download last launch', default='latest')
-    parser.add_argument('-dir', '--dir_path', help='directory where pictures will be saved, default="spacex_pictures"')
+    parser.add_argument('-dir', '--dir_path',
+                        help='directory where pictures will be saved, default="spacex_pictures"',
+                        default="spacex_pictures")
     app_args = parser.parse_args()
 
     urls = spacexAPI.get_imageurls_latest(f'https://api.spacexdata.com/v5/launches/{app_args.id}')
